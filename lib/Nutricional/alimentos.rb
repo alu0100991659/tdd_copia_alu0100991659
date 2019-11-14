b42be3b707b7ce65d5a84f68f39f8a769b8bfa7e
Eduardo Perez Hernandez <alu0100991659@ull.edu.es>
2019-11-14 22:49:37 +0000

    Prueba para porcion de alimento codificada

--- lib/Nutricional/alimentos.rb
+++ lib/Nutricional/alimentos.rb
@@ -1,6 +1,7 @@
 class Alimentos
-  attr_reader :nombre
+  attr_reader :nombre, :nPorciones
   def initialize(nombre,proteinas,hCarbono,lipidos,gei,terreno,nPorciones)
     @nombre = nombre
+    @nPorciones = nPorciones
   end
 end
