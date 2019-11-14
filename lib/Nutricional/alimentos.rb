2ba0e61640431fe2aaf9a834cb0ea874fc6beded
Eduardo Perez Hernandez <alu0100991659@ull.edu.es>
2019-11-14 23:00:42 +0000

    Prueba para GEI de alimento codificada

--- lib/Nutricional/alimentos.rb
+++ lib/Nutricional/alimentos.rb
@@ -1,10 +1,11 @@
 class Alimentos
-  attr_reader :nombre, :nPorciones, :proteinas, :carbohidratos, :lipidos
+  attr_reader :nombre, :nPorciones, :proteinas, :carbohidratos, :lipidos, :gei
   def initialize(nombre,proteinas,carbohidratos,lipidos,gei,terreno,nPorciones)
     @nombre = nombre
     @nPorciones = nPorciones
     @proteinas = proteinas*nPorciones
     @carbohidratos = carbohidratos*nPorciones
     @lipidos = lipidos*nPorciones
+    @gei = gei*nPorciones
   end
 end
