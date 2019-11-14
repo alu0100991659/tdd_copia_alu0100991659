bf0c3a56accdd859f72e5f81f837b7225f48c7cc
Eduardo Perez Hernandez <alu0100991659@ull.edu.es>
2019-11-14 22:49:15 +0000

    Prueba para porcion de alimento definida

--- spec/alimentos_spec.rb
+++ spec/alimentos_spec.rb
@@ -2,12 +2,15 @@ require 'spec_helper'
 
 RSpec.describe Alimentos do
   before :all do
-    @carneVaca = Alimentos.new("Carne de vaca", 21.1, 0.0, 3.1, 50.0, 164.0, 1)
+    @carneVaca = Alimentos.new("Carne de vaca", 21.1, 0.0, 3.1, 50.0, 164.0, 0.5)
   end
 
   context "Pruebas básicas para los alimentos" do
     it "Tiene nombre" do
       expect(@carneVaca).to have_attributes(:nombre => "Carne de vaca")
+    end
+    it "Tiene porciones" do
+      expect(@carneVaca).to have_attributes(:nPorciones => 0.5)
     end
   end
 end
