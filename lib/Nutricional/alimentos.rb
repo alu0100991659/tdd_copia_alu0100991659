c6fe2afb5f265557c507359ff3da96676b0db78c
Eduardo Perez Hernandez <alu0100991659@ull.edu.es>
2019-11-14 22:56:51 +0000

    Prueba para lipidos de alimento codificada

--- lib/Nutricional/alimentos.rb
+++ lib/Nutricional/alimentos.rb
@@ -1,9 +1,10 @@
 class Alimentos
-  attr_reader :nombre, :nPorciones, :proteinas, :carbohidratos
+  attr_reader :nombre, :nPorciones, :proteinas, :carbohidratos, :lipidos
   def initialize(nombre,proteinas,carbohidratos,lipidos,gei,terreno,nPorciones)
     @nombre = nombre
     @nPorciones = nPorciones
     @proteinas = proteinas*nPorciones
     @carbohidratos = carbohidratos*nPorciones
+    @lipidos = lipidos*nPorciones
   end
 end
