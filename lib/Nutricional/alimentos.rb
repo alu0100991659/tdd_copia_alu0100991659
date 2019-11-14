31e92d1d075b9dedcf77f29b99753db251845d03
Eduardo Perez Hernandez <alu0100991659@ull.edu.es>
2019-11-14 22:54:57 +0000

    Prueba para carbohidratos de alimento codificada

--- lib/Nutricional/alimentos.rb
+++ lib/Nutricional/alimentos.rb
@@ -1,8 +1,9 @@
 class Alimentos
-  attr_reader :nombre, :nPorciones, :proteinas
+  attr_reader :nombre, :nPorciones, :proteinas, :carbohidratos
   def initialize(nombre,proteinas,carbohidratos,lipidos,gei,terreno,nPorciones)
     @nombre = nombre
     @nPorciones = nPorciones
     @proteinas = proteinas*nPorciones
+    @carbohidratos = carbohidratos*nPorciones
   end
 end
