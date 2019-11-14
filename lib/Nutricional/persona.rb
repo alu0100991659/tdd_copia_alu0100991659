80f63cc952f06c47cf8412a397b1666f73f5c613
Eduardo Perez Hernandez <alu0100991659@ull.edu.es>
2019-11-14 23:53:03 +0000

    Prueba para sexo de persona codificada

--- lib/Nutricional/persona.rb
+++ lib/Nutricional/persona.rb
@@ -1,8 +1,9 @@
 require 'Nutricional/alimentos.rb'
 
 class Persona
-  attr_reader :nombre
+  attr_reader :nombre, :sexo
   def initialize(nombre,sexo)
     @nombre = nombre
+    @sexo = sexo
   end
 end
