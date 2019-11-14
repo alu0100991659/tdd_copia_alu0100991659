5f507f5174943b39774d10aa3ffb6a25890d34f8
Eduardo Perez Hernandez <alu0100991659@ull.edu.es>
2019-11-14 22:52:06 +0000

    Prueba para proteinas de alimento codificada

--- lib/Nutricional/alimentos.rb
+++ lib/Nutricional/alimentos.rb
@@ -1,7 +1,8 @@
 class Alimentos
-  attr_reader :nombre, :nPorciones
+  attr_reader :nombre, :nPorciones, :proteinas
   def initialize(nombre,proteinas,hCarbono,lipidos,gei,terreno,nPorciones)
     @nombre = nombre
     @nPorciones = nPorciones
+    @proteinas = proteinas*nPorciones
   end
 end
