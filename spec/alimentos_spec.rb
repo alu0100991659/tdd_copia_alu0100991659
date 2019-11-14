70b2a45026a5a0ceb548ea4c41adf84e6387acdd
Eduardo Perez Hernandez <alu0100991659@ull.edu.es>
2019-11-14 22:39:42 +0000

    Contexto para pruebas con alimentos definido

--- spec/alimentos_spec.rb
+++ spec/alimentos_spec.rb
@@ -1,1 +1,10 @@
 require 'spec_helper'
+
+RSpec.describe Alimentos do
+  before :all do
+    @carneVaca = Alimentos.new("Carne de vaca", 21.1, 0.0, 3.1, 50.0, 164.0, 1)
+  end
+
+  context "Pruebas básicas para los alimentos" do
+  end
+end
