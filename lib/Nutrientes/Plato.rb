05870816721cbf1b12aaef8c34f3819e1cd9a14e
Eduardo Perez Hernandez <alu0100991659@ull.edu.es>
2019-11-29 12:39:52 +0000

    Codificada prueba para obtener el porcentaje de proteinas de un plaro

--- lib/Nutrientes/Plato.rb
+++ lib/Nutrientes/Plato.rb
@@ -3,6 +3,14 @@ require "Nutrientes/version"
 class Plato < Lista
     include Comparable
 
+    def p_proteinas
+        x = collect{|a| 100.0*a.nPorciones}
+        y = collect{|a| a.proteinas}
+        gramosTotales = x.inject(0.0, :+)
+        gramosProteinas = y.inject(0.0, :+)
+        return ((gramosProteinas*100)/gramosTotales).round(3)
+    end
+
     def to_s
       x = collect{|a| a.nombre + ", "}
       y = x.inject("", :+)
