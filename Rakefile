c913c910c20d97a5339eb35da6ae4b3a5ae93bac
Eduardo Perez Hernandez <alu0100991659@ull.edu.es>
2019-11-29 10:38:59 +0000

    commit inicial practica 8

--- Rakefile
+++ Rakefile
@@ -1,6 +1,2 @@
+require "bundler/gem_tasks"
 task :default => :spec
-
-desc "Ejecutar las espectativas de la clase Alimento"
-task :spec do
-  sh "rspec -I. spec/alimentos_spec.rb"
-end
---
+++ lib/Nutrientes/Lista.rb
@@ -0,0 +1,69 @@
+require "NutrientesEdu/version"
+
+ Node = Struct.new(:value, :next, :prev)
+
+  class Lista
+    
+    include Comparable
+    attr_reader :head, :tail
+
+    def push_head(value)
+      if(@head == nil)
+        node=Node.new(value,nil,nil)
+        @head = node
+        @tail = node
+
+      else
+        node=Node.new(value,@head,nil)
+        @head.prev = node
+        @head = node
+
+      end
+    end
+
+    def push_tail(value)
+      if(@tail == nil)
+        node=Node.new(value,nil,nil)
+        @head = node
+        @tail = node
+      else
+        node=Node.new(value,nil,@tail)
+        @tail.next = node
+        @tail = node
+      end
+    end
+
+    def pop_head()
+      if(@head == nil)
+        return nil
+      else
+        val= @head
+        node = @head.next
+        @head = node
+        if @head != nil
+          @head.prev = nil
+        end
+        return val
+      end
+    end
+
+    def pop_tail()
+      if(@tail == nil)
+        return nil
+      else
+        val = @tail
+        node = @tail.prev
+        @tail = node
+        if @tail != nil
+          @tail.next = nil
+        end
+        return val
+      end
+    end
+
+    def to_s
+        s = ""
+        each {|i| s += "#{i.to_s}\n"}
+        s
+    end
+  end
