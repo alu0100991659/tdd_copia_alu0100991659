c472da90d177aee348eac7b1eef0ff3a57c19278
Eduardo Perez Hernandez <alu0100991659@ull.edu.es>
2019-12-20 03:39:35 +0000

    Codificada prueba para calcular plato de maxima huella ambiental de un menu

--- lib/Nutrientes/PlatoHuella.rb
+++ lib/Nutrientes/PlatoHuella.rb
@@ -1,7 +1,8 @@
 require "Nutrientes/version"
 
 class PlatoHuella < Plato
-  attr_reader :geiTotal, :terrenoTotal
+
+    attr_reader :geiTotal, :terrenoTotal
 
     def initialize(nombre,ingredientes)
         super(nombre,ingredientes)
@@ -12,6 +13,32 @@ class PlatoHuella < Plato
           @geiTotal = @geiTotal + a.gei
           @terrenoTotal = @terrenoTotal + a.terreno
         }
+    end
+
+    def huellaAmbiental
+      indiceCarbono = 0.0
+      if @geiTotal < 800.0
+        indiceCarbono = 1.0
+      elsif @geiTotal <= 1200.0
+        indiceCarbono = 2.0
+      else
+        indiceCarbono = 3.0
+      end
+
+      indiceEnergia = 0.0
+      if kcalPlato < 670.0
+        calorias = 1.00
+      elsif kcalPlato <= 830.0
+        calorias = 2.0
+      else
+        calorias = 3.0
+      end
+
+      return (indiceCarbono + indiceEnergia)/2
+    end
+
+    def <=> (other)
+      huellaAmbiental <=> other.huellaAmbiental
     end
 
     def to_s
