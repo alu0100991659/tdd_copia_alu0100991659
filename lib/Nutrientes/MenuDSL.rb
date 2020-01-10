a02bf1731fb049c5f5b6705da1c64bf483ad1e05
Eduardo Perez Hernandez <alu0100991659@ull.edu.es>
2020-01-10 12:18:01 +0000

    Prueba para obtener etiqueta formateada de menu DSL codificada

--- lib/Nutrientes/MenuDSL.rb
+++ lib/Nutrientes/MenuDSL.rb
@@ -1,5 +1,50 @@
 require "Nutrientes/version"
 
 class MenuDSL
+  include Enumerable
+
+  attr_reader :nombreMenu, :descripcionMenu, :componentes, :precios, :energias, :emisiones
+
+  def initialize(nombre, &block)
+      @nombreMenu = nombre
+      @alimentos = []
+      @componentes = []
+      @precios = []
+      @kcal =[]
+      @huella = []
+
+      if block_given?
+          if block.arity == 1
+              yield self
+          else
+              instance_eval(&block)
+          end
+      end
+  end
+
+  def to_s
+      "#{@nombreMenu} de #{@descripcionMenu}, Platos: #{@componentes.join(', ')}, Precio: #{@precios.reduce(:+)}, Kcal: #{@kcal.reduce(:+).round(1)}, Huella Ambiental: #{@huella.reduce(:+).round(1)}"
+  end
+
+  def descripcion(descripcion)
+    @descripcionMenu = descripcion
+  end
+
+  def componente(options = {})
+    componente = options[:plato]
+    @huella << componente.huellaDSL
+    @kcal << componente.kcalDSL
+
+    nombreDesc = componente.nombre
+    nombrePlato = componente.nombrePlato
+    precioPlato = options[:precio]
+    @precios << precioPlato
+
+    componenteFinal = " #{nombreDesc}:"
+    componenteFinal << " #{nombrePlato}"
+    componenteFinal << " (#{precioPlato})"
+
+    @componentes << componenteFinal
+  end
 
 end
